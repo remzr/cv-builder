e5eb2a1b87f2771d930cf68dc620aaf9c7346be9
remzr <110097561+remzr@users.noreply.github.com>
2025-12-24 14:45:30 +0100

    Change default form behaviour to react style form practise with state-functions.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,29 +1,51 @@
 import { useState } from "react";
 
 export default function Section() {
+    const [preName, setPreName] = useState("");
+    const [familyName, setFamilyName] = useState("");
+    const [mailAdress, setMailAdress] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+
+    function handleChange(e) {
+        let targetField = e.target.value;
+        console.log(e.target.value);
+
+        switch (e.target.id) {
+            case "preName":
+                setPreName(targetField);
+                break;
+            case "familyName":
+                setFamilyName(targetField);
+                break;
+            case "mailAdress":
+                setMailAdress(targetField);
+                break;
+            case "phoneNumber":
+                setPhoneNumber(targetField);
+                break;                
+        }
+    }
+
     return(
         <form>
             <h2>Allgemeine Angaben</h2>
             <fieldset>
-                <p>
-                    <label for="name"> Vorname:</label>
-                    <input type="text" id="preName" name="preName" />
-                </p>
-                <p>
-                    <label for="name"> Nachname:</label>
-                    <input type="text" id="familyName" name="familyName" />
-                </p>
-                <p>
-                    <label for="email"> E-Mail:</label>
-                    <input type="email" id="email" name="email" />
-                </p>
-                <p>
-                    <label for="tel"> Telefon:</label>
-                    <input type="tel" id="tel" name="tel" />
-                </p>
+                    <label for="name"> Vorname:
+                        <input type="text" id="preName" value={preName} onChange={handleChange} />
+                    </label>
+                    <label for="name"> Nachname:
+                        <input type="text" id="familyName" value={familyName} onChange={handleChange} />
+                    </label>
+                    <label for="email"> E-Mail:
+                        <input type="email" id="mailAdress" value={mailAdress} onChange={handleChange} />
+                    </label>
+                    <label for="tel"> Telefon:
+                        <input type="tel" id="phoneNumber" value={phoneNumber} onChange={handleChange} />
+                    </label>
                 <p>
                     <input class="submitButton" type="submit" value="Speichern" />
                 </p>
             </fieldset>
         </form>
 )};
+
